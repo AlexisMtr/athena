1b7cfb5c46754986c2e766f57a525e33b2c36e9a
Alexis Martinier <alexis.martinier@viseo.com>
2022-04-20 11:05:08 +0200

    refactor(configuration): extract forward logic

---
+++ src/Services/IDeviceForwarder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Athena.Models;
+
+namespace Athena.Services;
+public interface IDeviceForwarder
+{
+    Task ForwardDeviceConfiguration(DeviceConfiguration deviceConfiguration, Func<DeviceConfiguration, bool> forward);
+    Task ForwardDeviceConfiguration(DeviceConfiguration deviceConfiguration,bool forward = false);
+}
+
+public class EmptyDeviceForwarder : IDeviceForwarder
+{
+    public Task ForwardDeviceConfiguration(DeviceConfiguration deviceConfiguration, Func<DeviceConfiguration, bool> forward)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task ForwardDeviceConfiguration(DeviceConfiguration deviceConfiguration, bool forward = false)
+    {
+        return ForwardDeviceConfiguration(deviceConfiguration, c => forward);
+    }
+}
